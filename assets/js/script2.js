924f8c786319895e0ba5ed6af2cd56b0cef369e5
juliusbuckley <juliusbuckley@gmail.com>
2015-10-22 00:42:54 -0400

    cal tracker updates

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,7 +1,13 @@
 $(function (){
 
     // create global variables
-    var userInput,newUser,activityConstants = {"none":1.2,"low":1.375,"mod":1.55,"high":1.725,"extreme":1.9};
+    var userInput,userInput2,newUser,newFood,update = false,activityConstants = {"none":1.2,"low":1.375,"mod":1.55,"high":1.725,"extreme":1.9},foodList = [];
+
+    // get data about our products from workouts.json using ajax
+	$.getJSON("food.json",function(data) {
+		// add array from food.json to global varibale
+		foodList = data;
+	});
 
     // makes gender radio hidden and addes highlight color
     $('#gender-color input:radio').addClass('input_hidden');
@@ -19,21 +25,46 @@ $(function (){
         $(this).removeClass("highlight-button");
     });
 
+    // refresh page
+    $('.form-reset').on("click","#button",function(){
+        location.reload();
+    });
+
+    // adds highlight class while mouseenter button
+    $('.form-reset').on("mouseenter",function(){
+        $(this).addClass("highlight-button");
+    });
+
+    // removes highlight class while mouseenter button
+    $('.form-reset').on("mouseleave",function(){
+        $(this).removeClass("highlight-button");
+    });
+
+    // adds highlight class while mouseenter button
+    $('.form-btn').on("mouseenter",function(){
+        $(this).addClass("highlight-button");
+    });
+
+    // removes highlight class while mouseenter button
+    $('.form-btn').on("mouseleave",function(){
+        $(this).removeClass("highlight-button");
+    });
+
     // creates a new person object with serialized array
-    function createUser(array){
-        var user = {};
+    function createObject(array){
+        var object = {};
         array.forEach(function(val){
-            if(val.name === "age" || val.name === "weight" || val.name === "feet" || val.name ==="inches"){
+            if(val.name === "age" || val.name === "weight" || val.name === "feet" || val.name ==="inches" || val.name ==="cal" || val.name ==="fat" || val.name ==="sodium" || val.name ==="carbs" || val.name ==="protein" || val.name ==="serving"){
                 var numVal = Number(val.value);
-                user[val.name] = numVal;
+                object[val.name] = numVal;
             }
             else {
-                user[val.name] = val.value;
+                object[val.name] = val.value;
             }
         });
-        return user;
+        return object;
     }
-    
+
     // calculates basal metabolic rate
     function calcBMR(gender,feet,inches,weight,age,callback){
         var bmi = 0;
@@ -56,14 +87,76 @@ $(function (){
         return Math.floor(activityConstants[level]*callback);
     }
 
+    // updates calorie information once
+    function updateCalories(){
+        if(!update){
+            $(".footer .maintain").append(maintainWeight(newUser.level,calcBMR(newUser.gender,newUser.feet,newUser.inches,newUser.weight,newUser.age,totalInches)));
+            $(".footer .lose").append(maintainWeight(newUser.level,calcBMR(newUser.gender,newUser.feet,newUser.inches,newUser.weight,newUser.age,totalInches))-500);
+            $(".footer .gain").append(maintainWeight(newUser.level,calcBMR(newUser.gender,newUser.feet,newUser.inches,newUser.weight,newUser.age,totalInches))+500);
+        }
+        update = true;
+    }
+
+    // if value is NaN return value; else return value with number function applied
+    function numTest(value){
+        if(isNaN(value)){
+            return value;
+        }
+        else{
+            return Number(value);
+        }
+    }
+
+    // filters array by form values
+    function filterFood(array){
+        var filterArray = foodList.filter(function(value){
+            return value.group === array[0][1] && value.calories <= array[1][1] && value.fat <= array[2][1] && value.sodium <= array[3][1] && value.carbs <= array[4][1] && value.protein <= array[5][1] && value.serving <= array[6][1];
+        });
+        // if array is empty return message
+        if(filterArray.length===0){
+            return "No results within filter criteria...";
+        }
+        return filterArray;
+    }
+
+    // html is created for each element in array
+    function updateFood(array){
+        $(".food-description").remove();
+        var listOfFood = "";
+        array.forEach(function(food){
+            listOfFood +=  '<ul class="food-description">'+
+                                '<li><span class="red">Type: </span>'+food.group+'</li>'+
+                                '<li><span class="white">Name: </span>'+food.name+'</li>'+
+ 						        '<li><span class="blue">Calories: </span>'+food.calories+'</li>'+
+                                '<li><span class="blue">Fat: </span>'+food.fat+' grams</li>'+
+                                '<li><span class="blue">Sodium: </span>'+food.sodium+' milligrams</li>'+
+						        '<li><span class="green">Carbs: </span>'+food.carbs+' grams</li>'+
+                                '<li><span class="green">Protein: </span>'+food.protein+' grams</li>'+
+                                '<li><span class="green">Serving Size: </span>'+food.serving+' ounces</li>'+
+                            '</ul>';
+
+        });
+        return listOfFood;
+    }
+
     // on submit encode form elements as an array of names and values
-    $("form").submit(function(event){
+    $(".cal-form").submit(function(event){
         userInput = $(this).serializeArray();
-        newUser = createUser(userInput);
-        console.log(newUser);
-        console.log(maintainWeight(newUser.level,calcBMR(newUser.gender,newUser.feet,newUser.inches,newUser.weight,newUser.age,totalInches)));
+        newUser = createObject(userInput);
+        updateCalories();
         event.preventDefault();
     });
 
+    // sends form data to filter
+    $(".food-form").submit(function(event){
+        userInput2 = $(this).serializeArray();
+        newFood = createObject(userInput2);
+        var foodArray = $.map(newFood,function(value,index){
+            return [[index,numTest(value)]];
+        });
+        //console.log(filterFood(foodArray));
+        $(".food-list").append(updateFood(filterFood(foodArray)));
+        event.preventDefault();
+    });
 
 });
